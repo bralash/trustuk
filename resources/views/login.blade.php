c35e8ca9009a32c4b309aa71dc9fc94629ea612d
Emmanuel Asaber <emmanuelasaber@gmail.com>
2017-01-25 02:20:03 +0000

    login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,2 +1,43 @@
 @extends('layout.app')
 @section('page_title', 'Login')
+@section('panel_style', 'top_panel_style_7')
+@section('hero')
+    <section class="top_panel_image top_panel_image_1" style="background-image: url(../images/faq.jpg);">
+        <div class="top_panel_image_hover"></div>
+        <div class="top_panel_image_header">
+            <h1 class="top_panel_image_title">Login</h1>
+            <div class="breadcrumbs">
+                <a class="breadcrumbs_item home" href="{{URL::to('/')}}">Home</a> <span class="breadcrumbs_delimiter"></span> <span class="breadcrumbs_item current">Login</span>
+            </div>
+        </div>
+    </section>
+@endsection
+@section('welcome')
+    <div class="sc_section custom_bg_2">
+        <div class="content_wrap">
+            <div class="sc_empty_space" data-height="2.2em"></div>
+            <div class="sc_section margin_top_huge margin_bottom_huge">
+                <h2 class="sc_section_title sc_item_title">Login</h2>
+                <div class="login_form_wrapper">
+                    <form action="/auth/login" method="post">
+                        {{csrf_field()}}
+
+                        <div class="field">
+                            <label for="email">Email</label>
+                            <input type="email" name="email" id="email">
+                        </div>
+
+                        <div class="field">
+                            <label for="password">Password</label>
+                            <input type="password" name="password" id="password">
+                        </div>
+
+
+                        <button type="submit">Login</button>
+                    </form>
+                </div>
+            </div>
+            <div class="sc_empty_space" data-height="2em"></div>
+        </div>
+    </div>
+@endsection
