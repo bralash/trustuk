adea1fa0e8e693e8a4593dab7eb9dabf8df1fa68
Emmanuel Asaber <emmanuelasaber@gmail.com>
2017-02-06 16:12:30 +0000

    IPN

--- routes/web.php
+++ routes/web.php
@@ -39,4 +39,4 @@ Route::group(['prefix' => 'admin'], function() {
 });
 
 //For Internt Payment Notification
-Route::get('/notify', ['uses' => 'AuthController@notify']);
+Route::get('/notify', ['uses' => 'IPNController@index']);
