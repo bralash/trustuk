32cad21eab42b2fde26a9c844e106669a9efc26d
Emmanuel Asaber <emmanuelasaber@gmail.com>
2017-02-06 15:11:28 +0000

    Clean up

--- config/app.php
+++ config/app.php
@@ -162,7 +162,6 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        oteroweb\LaravelCoinPayment\LaravelCoinPaymentServiceProvider::class,
 
         /*
          * Package Service Providers...
@@ -226,7 +225,6 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'Coinpayment' => oteroweb\LaravelCoinPayment\CoinPaymentsAPI::class,
 
     ],
 
