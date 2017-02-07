23b7d9b4ac81026beeac1d533d6ecef93d0603b4
Emmanuel Asaber <emmanuelasaber@gmail.com>
2017-02-07 11:20:58 +0000

    dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,9 +9,17 @@
                 <span class="label">Total Investment</span>
             </div>
         </div>
+
         <div class="column">
             <div class="stat blue-shade">
-                <span class="top">${{$deposit->amount}}</span>
+                <span class="top">
+                    @if($deposit)
+                    ${{$deposit->amount}}
+                    @else
+                    $0.00
+                    @endif
+                </span>
+
                 <span class="label">Current Investment</span>
             </div>
         </div>
