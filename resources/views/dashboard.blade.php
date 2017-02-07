8aebcfe1cf5ca2ded7b0cd425c6e0e9e48285ae7
Emmanuel Asaber <emmanuelasaber@gmail.com>
2017-02-07 11:22:11 +0000

    Chages

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,7 +5,13 @@
     <div class="ui three column grid">
         <div class="column">
             <div class="stat light-blue">
-                <span class="top">$300.00</span>
+                <span class="top">
+                    @if($deposit)
+                        ${{$deposit->amount}}
+                    @else
+                        $0.00
+                    @endif
+                </span>
                 <span class="label">Total Investment</span>
             </div>
         </div>
@@ -25,7 +31,13 @@
         </div>
         <div class="column">
             <div class="stat green-shade">
-                <span class="top">$200.00</span>
+                <span class="top">
+                    @if($deposit)
+                        ${{$deposit->amount}}
+                    @else
+                        $0.00
+                    @endif
+                </span>
                 <span class="label">Expected Income</span>
             </div>
         </div>
