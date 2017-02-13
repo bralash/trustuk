2be3835df3c59e555ec7896051be9825ae3e11a9
Emmanuel Asaber <emmanuelasaber@gmail.com>
2017-02-13 14:10:13 +0000

    Address

--- resources/views/make-deposit.blade.php
+++ resources/views/make-deposit.blade.php
@@ -5,31 +5,13 @@
     <div class="deposit-wrapper">
         <form action="/admin/deposit" method="post">
             {{csrf_field()}}
-            <div class="field">
-                <label for="plan">Choose a plan</label>
-                <select name="plan" id="plan">
-                    <option value="TUF Exchange Traded Fund">TUF Exchange Traded Fund</option>
-                    <option value="TUF Starter">TUF Starter</option>
-                    <option value="TUF Fixed Maturity">TUF Fixed Maturity</option>
-                    <option value="Equity Fund of Funds">Equity Fund of Funds</option>
-                </select>
-            </div>
-            <div class="field">
-                <label for="pm">Payment Method</label>
-                <select name="payment_method" id="pm">
-                    <option value="bitcoin">Bitcoin</option>
-                </select>
-            </div>
-            <div class="field">
-                <label for="amount">Amount (in USD)</label>
-                <input type="text" name="amount" id="amount">
-            </div>
+
             <div class="field">
                 <span>Please pay to the bitcoin address <b>17S6JEtYEM48FqEufJarqSgP4xfcMoMV4N</b></span>
                 <img src="http://i.imgur.com/ODC8i15.png" alt="">
             </div>
 
-            <button class="button ui blue" type="submit">Deposit</button>
+            {{--<button class="button ui blue" type="submit">Deposit</button>--}}
         </form>
     </div>
 @endsection
