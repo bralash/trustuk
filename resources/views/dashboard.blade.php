19d38811a167c95ab0c694e8c258b6c48573af13
Emmanuel Asaber <emmanuelasaber@gmail.com>
2017-02-18 10:20:41 +0000

    Stats

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,44 +2,5 @@
 @section('page_title', 'Dashboard')
 @section('title', 'Dashboard')
 @section('stats')
-    <div class="ui three column grid">
-        <div class="column">
-            <div class="stat light-blue">
-                <span class="top">
-                    @if($deposit)
-                        $0.00
-                    @else
-                        $0.00
-                    @endif
-                </span>
-                <span class="label">Total Investment</span>
-            </div>
-        </div>
-
-        <div class="column">
-            <div class="stat blue-shade">
-                <span class="top">
-                    @if($deposit)
-                    ${{$deposit->amount}}
-                    @else
-                    $0.00
-                    @endif
-                </span>
-
-                <span class="label">Current Investment</span>
-            </div>
-        </div>
-        <div class="column">
-            <div class="stat green-shade">
-                <span class="top">
-                    @if($deposit)
-                        $0.00
-                    @else
-                        $0.00
-                    @endif
-                </span>
-                <span class="label">Expected Income</span>
-            </div>
-        </div>
-    </div>
+    
 @endsection
